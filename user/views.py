e33a2e093c3c7e027c0e706343fc6b96f086ec48
behiyekubrayilmaz@gmail.com <46108374+behiyekubrayilmaz@users.noreply.github.com>
2020-04-30 18:55:11 +0300

    Added user panel

---
+++ user/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from event.models import Category
+from home.models import Setting, UserProfile
+
+
+def index(request):
+    setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    current_user = request.user
+    profile = UserProfile.objects.get(pk=current_user.id)
+    context = {'category': category,
+               'setting': setting,
+               'profile': profile,
+                }
+    return render(request,"user_profile.html",context)
